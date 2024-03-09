05897059f349c5874bc5a746e8d3de79d8f8eda9
raoqais <160421315+raoqais@users.noreply.github.com>
2024-03-09 15:56:31 +0500

    Add files via upload

---
+++ class3/age.ts
@@ -0,0 +1,6 @@
+// let firstname='mohsin';
+// let age=23;
+// console.log('my name is ${firstname} and my age is ${age}')
+let firstname = 'mohsin';
+let age = 23;
+console.log(`my name is ${firstname} and my age is ${age}`);
